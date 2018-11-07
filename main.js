39cf0f342af49ead66aec07780ac072e5a5df48b
mwk913 <mwk913@outlook.com>
2018-11-07 21:51:40 +0300

    Finish Etch A Sketch

---
+++ main.js
@@ -0,0 +1,53 @@
+addDivGrid(25)
+changeColorEventListener()
+//DEFAULT GRID SIZE
+function addDivGrid(gridSize) { 
+    for(i=0; i < gridSize*gridSize ; i++) {
+        let newDiv = document.createElement("div"); 
+        newDiv.classList.add("gridBlock");
+        let containerDiv = document.getElementById("container"); 
+        containerDiv.append(newDiv);
+    }
+  }
+//THIS IS USED TO ASK USER FOR GRID SIZE
+function getUserGrid() {
+    let userGridSize = prompt("Enter grid size");
+    resetGrid();
+    addDivGrid(userGridSize);
+    changeColorEventListener();
+    if (userGridSize > 0 && userGridSize <= 60){
+        let divWidth = document.querySelectorAll('.gridBlock');
+        divWidth.forEach((grid) => {
+            grid.style.width = Math.round((500 / (userGridSize)) * 100) / 100 + 'px';
+            grid.style.height = Math.round((500 / (userGridSize)) * 100) / 100 + 'px';                
+        });
+    }else if (userGridSize == undefined) {
+        addDivGrid(25);
+        changeColorEventListener();
+    }
+    else {
+        alert('Enter a number less than 61');
+        addDivGrid(25);
+        changeColorEventListener();
+    }
+  }
+//THIS IS USED TO CHANGE COLOR OF DIV BEING HOVERED
+function changeDivColor(e) {
+    let hoverColor = e.target.style;
+    hoverColor.backgroundColor = '#FF6462';
+  }
+//EVENT LISTENER FOR HOVER EFFECTS FOR GRID
+function changeColorEventListener() {
+    let coloredDiv= document.querySelectorAll(".gridBlock");
+    coloredDiv.forEach((e) => {
+        e.addEventListener("mouseover", changeDivColor)
+    }
+)}
+//RESETS THE GRID
+function resetGrid(){
+    let resetDiv = document.querySelectorAll('.gridBlock');
+    resetDiv.forEach((block) => {
+        let removeDiv = document.querySelector('.gridBlock');
+        block.parentNode.removeChild(removeDiv);
+    }
+)}
